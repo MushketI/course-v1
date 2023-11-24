dabd8d22eb70653ee330c07ce64f759122ce5781
Igor <Mushket52@gmail.com>
2023-11-24 12:56:29 +0200

    two commit

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "")
+let numberOfFilms
+
+//Запуск программы
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "")
+
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "")
+  }
+}
+
+start()
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -8,13 +19,54 @@ const personalMovieDB = {
   private: false,
 }
 
-const a = prompt("Один из последних просмотренных фильмов?", "")
-const b = prompt("На сколько оцените его?", "")
-const c = prompt("Один из последних просмотренных фильмов?", "")
-const d = prompt("На сколько оцените его?", "")
+//Проверка ввода
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", "")
+    const b = prompt("На сколько оцените его?", "")
 
-personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
-// a - будет ключ обьекта; b - значение ключа.
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+      personalMovieDB.movies[a] = b
+      console.log("good")
+    } else {
+      console.log("error")
+      i-- //оператор дикремента, по сути вернуть цикл назад
+    }
+  }
+}
 
-console.log(personalMovieDB)
+rememberMyFilms()
+
+//Ответ пользователя о просмотренных фильмах
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов")
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Классический зритель")
+  } else if (personalMovieDB.count >= 30) {
+    console.log("Киноман!!!!")
+  } else {
+    console.log("Произошла ошибка")
+  }
+}
+
+detectPersonalLevel()
+
+//Открыть база данных или нет
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB)
+  }
+}
+
+showMyDB(personalMovieDB.private)
+
+//Любимый жанр
+function writeYuorGenres() {
+  for (let i = 1; i <= 3; i++) {
+    const genre = prompt(`ваш любимый жанр под номером ${i}?`)
+    personalMovieDB.genres[i - 1] = genre
+  }
+}
+
+writeYuorGenres()
