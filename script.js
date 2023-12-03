f953b93547971ebacd8a66a896536fad8a14b618
Igor <Mushket52@gmail.com>
2023-12-03 18:18:47 +0200

    three commite

--- script.js
+++ script.js
@@ -1,72 +1,88 @@
-let numberOfFilms
-
-//Запуск программы
-function start() {
-  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "")
-
-  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "")
-  }
-}
-
-start()
-
 const personalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
   private: false,
-}
+  start: function () {
+    personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "")
 
-//Проверка ввода
-function rememberMyFilms() {
-  for (let i = 0; i < 2; i++) {
-    const a = prompt("Один из последних просмотренных фильмов?", "")
-    const b = prompt("На сколько оцените его?", "")
-
-    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-      personalMovieDB.movies[a] = b
-      console.log("good")
-    } else {
-      console.log("error")
-      i-- //оператор дикремента, по сути вернуть цикл назад
+    while (
+      personalMovieDB.count == "" ||
+      personalMovieDB.count == null ||
+      isNaN(personalMovieDB.count)
+    ) {
+      personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "")
     }
-  }
+  },
+  rememberMyFilms: function () {
+    for (let i = 0; i < 2; i++) {
+      const a = prompt("Один из последних просмотренных фильмов?", "")
+      const b = prompt("На сколько оцените его?", "")
+
+      if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b
+        console.log("good")
+      } else {
+        console.log("error")
+        i-- //оператор дикремента, по сути вернуть цикл назад
+      }
+    }
+  },
+  detectPersonalLevel: function () {
+    if (personalMovieDB.count < 10) {
+      console.log("Просмотрено довольно мало фильмов")
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+      console.log("Классический зритель")
+    } else if (personalMovieDB.count >= 30) {
+      console.log("Киноман!!!!")
+    } else {
+      console.log("Произошла ошибка")
+    }
+  },
+  showMyDB: function (hidden) {
+    if (!hidden) {
+      console.log(personalMovieDB)
+    }
+  },
+  writeYuorGenres: function () {
+    for (let i = 1; i <= 3; i++) {
+      const genre = prompt(`ваш любимый жанр под номером ${i}?`)
+      if (genre === null || genre === "") {
+        console.log("Error")
+        i--
+      } else {
+        personalMovieDB.genres[i - 1] = genre
+      }
+    }
+
+    personalMovieDB.genres.forEach((value, index) => {
+      console.log(`Любимый жанр #${index + 1} - это ${value}`)
+    })
+  },
+  toggleVisibleMyDB: function () {
+    if (personalMovieDB.private) {
+      personalMovieDB.private = false
+    } else {
+      personalMovieDB.private = true
+    }
+  },
 }
 
-rememberMyFilms()
+// //Запуск программы
+personalMovieDB.start()
 
-//Ответ пользователя о просмотренных фильмах
-function detectPersonalLevel() {
-  if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов")
-  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Классический зритель")
-  } else if (personalMovieDB.count >= 30) {
-    console.log("Киноман!!!!")
-  } else {
-    console.log("Произошла ошибка")
-  }
-}
+// //Проверка ввода
+personalMovieDB.rememberMyFilms()
 
-detectPersonalLevel()
+// //Ответ пользователя о просмотренных фильмах
+personalMovieDB.detectPersonalLevel()
 
 //Открыть база данных или нет
-function showMyDB(hidden) {
-  if (!hidden) {
-    console.log(personalMovieDB)
-  }
-}
-
-showMyDB(personalMovieDB.private)
+personalMovieDB.showMyDB(personalMovieDB.private)
 
 //Любимый жанр
-function writeYuorGenres() {
-  for (let i = 1; i <= 3; i++) {
-    const genre = prompt(`ваш любимый жанр под номером ${i}?`)
-    personalMovieDB.genres[i - 1] = genre
-  }
-}
+personalMovieDB.writeYuorGenres()
 
-writeYuorGenres()
+//Переключатель privat
+personalMovieDB.toggleVisibleMyDB()
